f280de4e8d13b44dd06dc34130a0cd7e6ca54ec9
Timothy Ademola <timife007@gmail.com>
2023-08-19 04:25:03 +0100

    movie repository impl

--- lib/data/repositories/movie_repo_impl.dart
+++ lib/data/repositories/movie_repo_impl.dart
@@ -7,3 +7,26 @@ import 'package:movies/data/remote/movies_api.dart';
 import 'package:movies/domain/models/movie.dart';
 import 'package:movies/domain/repositories/movie_repository.dart';
 
+class MovieRepositoryImpl extends MovieRepository {
+  final MoviesApi moviesApi;
+  MovieRepositoryImpl(this.moviesApi);
+  @override
+  Future<DataState<List<Movie>>> getMovies() async {
+    final httpResponse = await moviesApi.getDiscoverMovies();
+    try {
+      if (httpResponse.response.statusCode == HttpStatus.ok) {
+        return DataSuccess(httpResponse.data.moviesDto
+            .map((movieDto) => movieDto.toMovie())
+            .toList());
+      } else {
+        return DataFailed(DioException(
+            error: httpResponse.response.statusMessage,
+            response: httpResponse.response,
+            type: DioExceptionType.badResponse,
+            requestOptions: httpResponse.response.requestOptions));
+      }
+    } on DioException catch (e) {
+      return DataFailed(e);
+    }
+  }
+}
