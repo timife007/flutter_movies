0671247e63510711c794c23a8b28a41f8548d6e2
Timothy Ademola <timife007@gmail.com>
2023-08-21 10:54:20 +0100

    item favorite implemented

--- lib/presentation/widgets/movie_item.dart
+++ lib/presentation/widgets/movie_item.dart
@@ -2,9 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:movies/core/providers/dio/dio_provider.dart';
 import 'package:movies/domain/models/movie.dart';
 
-class MovieItem extends StatelessWidget {
+class MovieItem extends StatefulWidget {
   final Movie movie;
   const MovieItem(this.movie, {super.key});
+
+  @override
+  State<MovieItem> createState() => _MovieItemState();
+}
+
+class _MovieItemState extends State<MovieItem> {
+  bool _isFav = false;
+
+  void _changeFav() {
+    setState(() {
+      _isFav = !_isFav;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return InkWell(
@@ -23,22 +37,34 @@ class MovieItem extends StatelessWidget {
           children: [
             Expanded(
                 child: ClipRRect(
-              borderRadius: BorderRadius.circular(16),
-              child: Container(
-                height: 150,
-                decoration: BoxDecoration(
-                    image: DecorationImage(
-                        image: NetworkImage(imageBaseUrl + movie.posterPath),
-                        fit: BoxFit.fill)),
-              ),
-            )),
+                    borderRadius: BorderRadius.circular(16),
+                    child: Stack(
+                      children: [
+                        Container(
+                          height: 150,
+                          decoration: BoxDecoration(
+                              image: DecorationImage(
+                                  image: NetworkImage(
+                                      imageBaseUrl + widget.movie.posterPath),
+                                  fit: BoxFit.fill)),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(1.0),
+                          child: IconButton(
+                              onPressed: _changeFav,
+                              icon: _isFav
+                                  ? Icon(Icons.favorite)
+                                  : Icon(Icons.favorite_border_outlined)),
+                        )
+                      ],
+                    ))),
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: Text(
-                movie.title,
+                widget.movie.title,
                 style: const TextStyle(fontWeight: FontWeight.bold),
               ),
-            )
+            ),
           ],
         ),
       ),
